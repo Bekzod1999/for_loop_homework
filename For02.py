04e2a98c745b20c43b39e2935b85d19cdb24427c
Bekzod1999 <developmentfirm1999@gmail.com>
2022-08-30 20:17:41 +0500

    02

--- For02.py
+++ For02.py
@@ -1,3 +1,28 @@
+# def main(n):
+#     """
+#     Return numbers from zero to n in a string view.
+#     Args:
+#         n: int
+#     Returns:
+#         string: return  answer
+#     """
+#     list1 = ' '
+#     list = [] 
+#     for i in range(n):
+#         # list.append(i)
+#         if i == 0:
+#             list1 += '"' + str(i)
+#         elif i == n - 1:
+#             list1 += ',' + str(i) + '"'
+#         else:
+#             list1 += ','+ str(i)
+#     return str(list1)
+
+# x = main(30)
+# print(x)
+
+
+
 def main(n):
     """
     Return numbers from zero to n in a string view.
@@ -6,17 +31,14 @@ def main(n):
     Returns:
         string: return  answer
     """
-    list1 = ' '
+    list1 = ''
     list = [] 
+    m = ','
     for i in range(n):
-        # list.append(i)
-        if i == 0:
-            list1 += '"' + str(i)
-        elif i == n - 1:
-            list1 += ',' + str(i) + '"'
-        else:
-            list1 += ','+ str(i)
-    return str(list1)
+        list.append(str(i))
+        str(list)
+        k = m.join(list)
+    return k
 
-x = main(30)
+x = main(3)
 print(x)
